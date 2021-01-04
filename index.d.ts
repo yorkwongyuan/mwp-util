d163890aa9e3caca157f578337cc820fee4095d7
yorkwongyuan <631824375@qq.com>
2021-01-04 10:23:08 +0800

    add type

--- index.d.ts
+++ index.d.ts
@@ -1,5 +1,11 @@
 declare namespace mwpUtil{
-  export function insert(arr: array, index: number): any[]
+  export function insert(arr: [], index: number): any[]
+  export function append(arr: [], newArr: any): any[]
+  export function sum (arr: number[]): number
+  export function prepend (arr: [], options: any): any[]
+  export function remove(arr: [], index: number): any[]
+  export function reduceFn (callback: ()=> any): () => any[]
+  export function url (name: string): string
 }
 
 declare module "mwp-util" {
